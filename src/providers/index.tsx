20e01d3f69845e680bd8a0dfaa6ca4c920d2f20c
Gabriel Ramon <garamon97@gmail.com>
2026-05-13 00:19:18 -0300

    refactor: integra provedores de contexto com servicos de debug

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { ItemContextProvider } from './ItemContextProvider';
 import { CollectionContextProvider } from './CollectionContextProvider';
+import { PostContextProvider } from './PostContextProvider';
 
 export { ItemContextProvider, useItemService } from './ItemContextProvider';
 export { CollectionContextProvider, useCollectionService } from './CollectionContextProvider';
+export { PostContextProvider, usePostService } from './PostContextProvider';
 
 interface CreateItemCollectionProvidersProps {
   children: React.ReactNode;
 }
 
 /**
- * Wrapper component combining all item and collection providers
- * Use this to wrap your app or specific subtrees that need access to item/collection services
+ * Wrapper component combining all item, collection and post providers
+ * Use this to wrap your app or specific subtrees that need access to these services
  */
 export const createItemCollectionProviders = ({ children }: CreateItemCollectionProvidersProps) => (
-  <ItemContextProvider>
-    <CollectionContextProvider>{children}</CollectionContextProvider>
-  </ItemContextProvider>
+  <PostContextProvider>
+    <ItemContextProvider>
+      <CollectionContextProvider>{children}</CollectionContextProvider>
+    </ItemContextProvider>
+  </PostContextProvider>
 );
